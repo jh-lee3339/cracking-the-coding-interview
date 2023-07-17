0c98466fad4ae725a57b25df10b66408a77df2fe
jh-lee3339 <84605635+jh-lee3339@users.noreply.github.com>
2023-07-18 01:25:38 +0900

    Added Brute Force Solution

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -6,8 +6,21 @@ package arraystring;
 
 class _01_01_IsUnique {
 
+    /* 
+    Attempt 1: Brute Force Solution
+    Time: O(N^2)
+    Space: O(1)
+    */
     boolean isUnique(String str) {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
 
 }
