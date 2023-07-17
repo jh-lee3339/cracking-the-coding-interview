7eb6d73aa5423e2d1eeac9d030fbc6190ca15034
jh-lee3339 <84605635+jh-lee3339@users.noreply.github.com>
2023-07-18 01:37:20 +0900

    Added HashSet Solution

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -1,5 +1,8 @@
 package arraystring;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * Implement an algorithm to determine if a string has all unique characters.
  */
@@ -10,13 +13,28 @@ class _01_01_IsUnique {
     Attempt 1: Brute Force Solution
     Time: O(N^2)
     Space: O(1)
-    */
     boolean isUnique(String str) {
         for (int i = 0; i < str.length(); i++) {
             for (int j = i + 1; j < str.length(); j++) {
                 if (str.charAt(i) == str.charAt(j)) {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+    */
+
+    /*
+     * Attempt 2: HashSet
+     * Time: O(N)
+     * Space: O(N)
+     */
+    boolean isUnique(String str) {
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (!set.add(str.charAt(i))) {
+                return false;
             }
         }
         return true;
